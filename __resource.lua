f66b8cde144cec21aad71c37b50cecdbc6092f99
LuaDeldu <luadelduwaht@outlook.fr>
2017-11-02 16:38:16 +0100

    chore(resource): Add version

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Accessories'
 
-version '0.0.8'
+version '1.0.0'
 
 server_scripts {
     '@es_extended/locale.lua',
